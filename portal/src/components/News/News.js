fea83ccc2163347b6250c233d0430cbe6545d7f4
Valery Dluski <valerydluski@gmail.com>
2021-04-18 10:59:46 +0300

    Fetch data and update your state

--- portal/src/components/News/News.js
+++ portal/src/components/News/News.js
@@ -2,8 +2,30 @@ import React, { Component } from 'react';
 import NewSingle from './NewSingle';
 
 export default class News extends Component {
+  constructor(props) {
+    super(props);
+    this.state= {
+      news: [],
+    }
+  }
+
+  componentDidMount() {
+    const url ='https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=e2497413ff0b4b4db1cef731a0591bfc';
+
+    fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        this.setState({
+          news: data.articles
+        })
+      })
+      .catch((error) => console.log(error));
+  }
+
   renderItems() {
-    return this.props.items.map((item) => (
+    return this.state.news.map((item) => (
       <NewSingle key={item.id} item={item} />
     ))
   }
